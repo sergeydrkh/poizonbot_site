1569902ddae11b32594ceb582a184d473116735c
serezk4 <serezadorohin@gmail.com>
2023-04-11 21:05:19 +0700

    'update'

--- telegram_connection.js
+++ telegram_connection.js
@@ -4,10 +4,13 @@ function login() {
     const dewuLink = document.getElementById('link').value;
     const dewuSize = document.getElementById('size').value;
     const dewuCost = document.getElementById('cost').value;
-   
+	let params = new URLSearchParams(location.search);
+	const orderSalt = params.get('order_salt');
+	const menuId = params.get('menu_id');
 
-    Telegram.WebApp.sendData(`${dewuLink};${dewuSize};${dewuCost}`);    
+    Telegram.WebApp.sendData(`${menuId};addItem;${dewuLink};${dewuSize};${dewuCost};${orderSalt}`);    
 }
+
 
 Telegram.WebApp.ThemeParams.bg_color="#FFFFFF";
 Telegram.WebApp.ThemeParams.text_color="#FFFFFF";
